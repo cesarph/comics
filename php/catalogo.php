bf083ca62a2cb821b3f3fe235948f276af5ec59f
César Palazuelos <cesar_96_ph@hotmail.com>
2018-05-18 18:41:15 -0500

    Se añade estilo y funcionalidad al catalogo

--- php/catalogo.php
+++ php/catalogo.php
@@ -4,21 +4,24 @@
     $authors = mysqli_query($con, "SELECT * FROM autor;");
     $editorials = mysqli_query($con, "SELECT * FROM editorial;");
     
-
     if (isset($_GET['genre'])) {
         $genre = strtolower($_GET['genre']);
+        $active = $genre;
         $comics = mysqli_query($con, "SELECT c.id_comic, c.titulo, c.precio, c.imagen, a.nombre FROM comics c, autor a, genero g WHERE c.genero = g.id_genero AND c.autor = a.id_autor AND nombre_genero='$genre'");
 
     } else if (isset($_GET['author'])) {
         $author = strtolower($_GET['author']);
+        $active = $author;
         $comics = mysqli_query($con, "SELECT c.id_comic, c.titulo, c.precio, c.imagen, a.nombre FROM comics c, autor a WHERE c.autor = a.id_autor AND a.nombre='$author'");
 
     } else if (isset($_GET['editorial'])) {
         $editorial = strtolower($_GET['editorial']);
+        $active = $editorial;
         $comics = mysqli_query($con, "SELECT c.id_comic, c.titulo, c.precio, c.imagen, a.nombre FROM comics c, autor a, editorial e WHERE c.editorial = e.id_editorial AND c.autor = a.id_autor AND nombre_editorial='$editorial'");
 
     } else {
         $comics = mysqli_query($con, "SELECT c.id_comic, c.titulo, c.precio, c.imagen, a.nombre FROM comics c, autor a WHERE c.autor = a.id_autor");
+        $active = "all";
     }
         
     
@@ -45,76 +48,83 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Comics eShop</a>
+            <a class="navbar-brand" href="../index.php">Comics</a>
             
             </div>
     
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <div class="row">
-                    <div class="col-md-8"></div>
-                    <div class="col-md-4">
-                    <form action="navbar-form navbar-right">
-                        <div class="input-group form-group">
-                            <input type="text" class="form-control" placeholder="Buscar">
+                <ul class="nav navbar-nav">
+                    <li><a href="./catalogo.php">Catalogo</a></li>
+                    <form class="navbar-form navbar-right" action="./catalogo.php" method="GET">
+                        
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="titulo" placeholder="Buscar">
                             <span class="input-group-btn">
-                                <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                                <button class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
                             </span>
                         </div>
                     </form>
-                    </di>
-                </div> 
-                
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="./catalogo.php">Catalogo</a></li>
-                <li><a href="./carrito.php">Carrito</a></li>
-                <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mi cuenta<span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="#">Mi cuenta</a></li>
-                    <li><a href="#">Historial de compras</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Cerrar Sesión</a></li>
                 </ul>
-                </li>
-            </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    
+                    <?php if (isset($_SESSION['userID']) && $_SESSION['userID'] != "") { ?>
+                        <li><a href="./carrito.php">Carrito</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mi cuenta<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="./cuenta.php">Mi cuenta</a></li>
+                                <li><a href="./historial.php">Historial de compras</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="./cerrar-sesion.php">Cerrar Sesión</a></li>
+                            </ul>
+                        </li>
+                    <?php }  else { ?>
+                        <li><a href="./iniciar-sesion.php">Iniciar Sesión</a></li>
+                        <li><a href="./registro.php">Registrarse</a></li>
+                    <?php }?>
+                    
+                </ul>
             </div>
         </div>
     </nav>
-    <div class="container">
+    <div class="container container-catalog">
         <div class="row">
             <div class="col-md-3">
                 <ul class="nav nav-pills nav-stacked">
-                    <li role="presentation" class="active"><a href="./catalogo.php">All</a></li>
-                    <li>Generos</li>
+                    <li role="presentation" class="<?php if ($active == "all") active ?>"><a href="./catalogo.php">Todos</a></li>
+                    <li><strong>Generos</strong></li>
                     <?php while($row = mysqli_fetch_array($genres)) { ?>
-                        <li role="presentation"><a href="./catalogo.php?genre=<?php echo $row['nombre_genero']?>"><?php echo $row['nombre_genero']?></a></li>
+                        <li role="presentation" class="<?php if ($active == strtolower($row['nombre_genero'])) active ?>"><a href="./catalogo.php?genre=<?php echo $row['nombre_genero']?>"><?php echo $row['nombre_genero']?></a></li>
                     <?php    } ?>
-                    <li>Autores</li> 
+                    <li><strong>Autores</strong></li> 
                     <?php while($row = mysqli_fetch_array($authors)) { ?>
-                        <li role="presentation"><a href="./catalogo.php?author=<?php echo $row['nombre']?>"><?php echo $row['nombre']?></a></li>
+                        <li role="presentation" class="<?php if ($active == strtolower($row['nombre'])) active ?>"><a href="./catalogo.php?author=<?php echo $row['nombre']?>"><?php echo $row['nombre']?></a></li>
                     <?php    } ?>
-                    <li>Editoriales</li>
+                    <li><strong>Editoriales</strong></li>
                     <?php while($row = mysqli_fetch_array($editorials)) { ?>
-                        <li role="presentation"><a href="./catalogo.php?editorial=<?php echo $row['nombre_editorial']?>"><?php echo $row['nombre_editorial']?></a></li>
+                        <li role="presentation" class="<?php if ($active == strtolower($row['nombre_editorial'])) active ?>"><a href="./catalogo.php?editorial=<?php echo $row['nombre_editorial']?>"><?php echo $row['nombre_editorial']?></a></li>
                     <?php    } ?>
                     
                 </ul>
             </div>
             <div class="col-md-9">
                 <div class="comic-container row">
-                    <?php while($row = mysqli_fetch_array($comics)) { ?>
+                        
+                    <?php if (mysqli_num_rows($comics)) { 
+                        while($row = mysqli_fetch_array($comics)) { ?>
                         <div class="comic col-md-4">
                             <a href="./comic.php?id=<?php echo $row['id_comic'] ?>">
                                 <img class="img-thumbnail img-comic" src="<?php echo $row['imagen']?>" alt="<?php echo $row['titulo']?>">
-                                <p><?php echo $row['titulo'] ?></p>
+                                <p class="comic-title"><?php echo $row['titulo'] ?></p>
                             </a>
-                            <p>de <?php echo $row['nombre']?></p>
-                            <p>$<?php echo $row['precio']?></p>
+                            <p class="comic-author">de <?php echo $row['nombre']?></p>
+                            <p class="comic-price"><strong>Precio:</strong> $<?php echo $row['precio']?></p>
                             
                             
                         </div>
+                    <?php } 
+                    } else { ?>
+                        <p>No se encontraron comics</p>
                     <?php } ?>
                 </div>
             </div>
