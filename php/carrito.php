e9c4fe1b197dd4e1e4c24a7c90cf40da928f4edc
César Palazuelos <cesar_96_ph@hotmail.com>
2018-05-18 18:36:31 -0500

    Se añade el estilo y la funcionalidad al carrito

--- php/carrito.php
+++ php/carrito.php
@@ -9,19 +9,22 @@
                 break;
             case 'update':
                 $comicID = $_POST['id'];
-                $addToCart = mysqli_query($con, "INSERT INTO carrito (comic, usuario) VALUES($comicID, 1)");
+                $userID = $_SESSION['userID'];
+                $addToCart = mysqli_query($con, "INSERT INTO carrito (comic, usuario) VALUES($comicID, $userID)");
                 break;
         }
         
     }
     $suma = 0;
-    
-    $itemsInCart =  mysqli_query($con, "SELECT ca.id_carrito, c.titulo, c.precio, c.cantidad_en_almacen, c.imagen, a.nombre, g.nombre_genero, ca.usuario 
+    if(isset($_SESSION['userID']) && $_SESSION['userID'] != "" ){
+        $userID = $_SESSION['userID'];
+        $itemsInCart =  mysqli_query($con, "SELECT ca.id_carrito, c.titulo, c.precio, c.cantidad_en_almacen, c.imagen, a.nombre, g.nombre_genero, ca.usuario 
                                         FROM comics c, autor a, carrito ca, genero g, usuarios u 
-                                        WHERE c.autor = a.id_autor AND ca.usuario = u.id_usuario AND ca.comic = c.id_comic AND g.id_genero=c.genero AND ca.usuario=1");
+                                        WHERE c.autor = a.id_autor AND ca.usuario = u.id_usuario AND ca.comic = c.id_comic AND g.id_genero=c.genero AND ca.usuario=$userID");
     
-   
-    
+    } else {
+        header('Location: ../index.php');
+    }
 ?>
 
 <!DOCTYPE html>
@@ -30,13 +33,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Catalogo</title>
+    <title>Carrito</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/index.css">
     <link rel="stylesheet" href="../css/carrito.css">
 </head>
 <body>
-    <nav class="navbar navbar-inverse">
+    
+<nav class="navbar navbar-inverse">
         <div class="container">
             <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -45,89 +49,102 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Comics eShop</a>
+            <a class="navbar-brand" href="../index.php">Comics</a>
             
             </div>
     
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <div class="row">
-                    <div class="col-md-8"></div>
-                    <div class="col-md-4">
-                    <form action="navbar-form navbar-right">
-                        <div class="input-group form-group">
-                            <input type="text" class="form-control" placeholder="Buscar">
+                <ul class="nav navbar-nav">
+                    <li><a href="./catalogo.php">Catalogo</a></li>
+                    <form class="navbar-form navbar-right" action="./catalogo.php" method="GET">
+                        
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="titulo" placeholder="Buscar">
                             <span class="input-group-btn">
-                                <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                                <button class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
                             </span>
                         </div>
                     </form>
-                    </di>
-                </div> 
-                
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="./catalogo.php">Catalogo</a></li>
-                <li><a href="./carrito.php">Carrito</a></li>
-                <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mi cuenta<span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="#">Mi cuenta</a></li>
-                    <li><a href="#">Historial de compras</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Cerrar Sesión</a></li>
                 </ul>
-                </li>
-            </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    
+                    <?php if (isset($_SESSION['userID']) && $_SESSION['userID'] != "") { ?>
+                        <li><a href="./carrito.php">Carrito</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mi cuenta<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="./cuenta.php">Mi cuenta</a></li>
+                                <li><a href="./historial.php">Historial de compras</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="./cerrar-sesion.php">Cerrar Sesión</a></li>
+                            </ul>
+                        </li>
+                    <?php }  else { ?>
+                        <li><a href="./iniciar-sesion.php">Iniciar Sesión</a></li>
+                        <li><a href="./registro.php">Registrarse</a></li>
+                    <?php }?>
+                    
+                </ul>
             </div>
         </div>
     </nav>
     <div class="container">
-        <div class="row">
+        <div class="row"> 
             <div class="col-md-8">
                 <div class="cart-container">                    
                     <div class="row">
                             <div class="col-md-9">
-                                <p>Carrito</p>
+                                <strong>Carrito</strong>
                             </div>
                             <div class="col-md-3">
-                                <p>Precio </p>
+                                <strong>Precio </strong>
                             </div>
                     </div>
                     <hr>
-                    <?php while($row = mysqli_fetch_array($itemsInCart)) { $suma += $row['precio'] ?>
-                        <div class="cart-item">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <img class="img-thumbnail cart-item-img" src="<?php echo $row['imagen']?>" alt="">
+                    <?php if (isset($_SESSION['userID']) && $_SESSION['userID'] != "" && mysqli_num_rows($itemsInCart) ) { 
+                        while($row = mysqli_fetch_array($itemsInCart)) { $suma += $row['precio'] ?>
+                            <div class="cart-item">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img class="img-thumbnail cart-item-img" src="<?php echo $row['imagen']?>" alt="">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p><?php echo $row['titulo'] ?></p>
+                                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                                            <input type="hidden" name="method" value="delete">
+                                            <input type="hidden" name="id" value="<?php echo $row['id_carrito'] ?>">
+                                            <button class="btn btn-danger">eliminar</buttton>
+                                        </form>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <p>$<?php echo $row['precio'] ?> </p>
+                                    </div>
+                                    
                                 </div>
-                                <div class="col-md-6">
-                                    <p><?php echo $row['titulo'] ?></p>
-                                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-                                        <input type="hidden" name="method" value="delete">
-                                        <input type="hidden" name="id" value="<?php echo $row['id_carrito'] ?>">
-                                        <button class="btn btn-danger">eliminar</buttton>
-                                    </form>
-                                </div>
-                                <div class="col-md-3">
-                                    <p>$<?php echo $row['precio'] ?> </p>
-                                </div>
-                                
                             </div>
-                        </div>
-                    <?php    } ?>
-                   
+                    <?php    } 
+                         } else if (isset($_SESSION['userID']) && $_SESSION['userID'] != "") { ?>
+                            <p>Tu carrito está vacio</p>
+                    <?php } else { ?>
+                            <p>Tiene que tener cuenta para poder usar esta función</p>
+                    <?php } ?>
                 </div>
                 
             </div>
             <div class="col-md-4">
-                <form action="./checkout.php" method="POST">
-                    <input type="hidden" name="id" value="<?php echo $result['id_carrito']?>">
-                    <p>Total: $<?php echo $suma?></p>
-                    <button>Proceder al pago</button>
-                </form>
+                <div class="checkout-form">
+                    <form action="./checkout.php" method="POST">
+                        <p><strong>Total:</strong> $<?php echo $suma?></p>
+                        <input type="hidden" name="id" value="<?php echo $_SESSION['userID']?>">
+                        <?php if (isset($_SESSION['userID']) && $_SESSION['userID'] != "") { ?>
+                            <button class="btn btn-success">Proceder al pago</button>
+                        <?php } else { ?>
+                            <button class="btn btn-success" disabled>Proceder al pago</button>
+                            <p class="not-available">Para usar esta función debe tener una cuenta</p>
+                        <?php } ?>
+                    </form>
+                </div>
                 
-            
             </div>
         </div>
         
